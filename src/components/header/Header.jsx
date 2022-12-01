e7293dcb6c4baa761f994861a77a79ed4a3b6718
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2022-12-01 17:30:28 +0900

    헤더 작업

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './header.css'
 import CTA from "./CTA";
 import Me from "../../assets/me.png"
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -11,10 +12,12 @@ const Header = () => {
         <h1>SeongGyu</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={Me} alt="" />
         </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
