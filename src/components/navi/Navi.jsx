ce83257cddd02f7d965ef88c1cdf81135f8b18bd
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2022-12-02 11:47:54 +0900

    Navi 컴포넌트

--- src/components/navi/Navi.jsx
+++ src/components/navi/Navi.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import './navi.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from "react-icons/ri";
 
 const Navi = () => {
+  const [activeNavi, setActiveNavi] = useState('#')
+
   return (
-    <div>Navi</div>
+    <navi>
+      <a href="#" onClick={() => setActiveNavi('#')} className={activeNavi === '#'?"active":""} > <AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNavi('#about')} className={activeNavi === '#about'?"active":""}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNavi('#experience')} className={activeNavi === '#experience'?"active":""}><BiBook /></a>
+      <a href="#services" onClick={() => setActiveNavi('#services')}  className={activeNavi === '#services'?"active":""}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNavi('#contact')}  className={activeNavi === '#contact'?"active":""}><BiMessageSquareDetail /></a>
+    </navi>
   )
 }
 
