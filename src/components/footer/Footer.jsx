2ba829e3126d837254aca0fc6bb76ce7b58cd706
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2022-12-02 17:26:16 +0900

    오류 수정 및 빌드 전

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import './footer.css'
+import {AiFillFacebook,AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Dev_Kang</a>
+
+      <ul className="permalinks">
+        <li><a href="#"></a> </li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><AiFillFacebook /></a>
+        <a href="https://instagram.com"><AiFillInstagram /></a>
+        <a href="https://twitter.com"><AiOutlineTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Dev_Kang. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
